42860e6bf7a3bda2ec797a642d6730da435a0c1b
OverflowCat <overflowcat@gmail.com>
2020-12-13 01:18:14 +0800

    批

--- 心酸泪.py
+++ 心酸泪.py
@@ -9,8 +9,17 @@ for chap in chaps:
   print(chap)
   with codecs.open("荒唐言\\" + chap, 'r+', encoding='utf-8') as f:
     text = f.read()
-  text = (" <small>").join(text.split("```("))
-  text = ("</small> ").join(text.split(")```"))
+  annotations = [
+    ["```(甲戌双行夹批：", '<code class="jd">'],
+    ["```(蒙侧批：", '<code class="ms">'],
+    ["```(蒙：", '<code class="me">'],
+    ["```(庚辰侧批：", '<code class="gs">'],
+    ["```(庚辰双行夹批", '<code class="gd">'],
+    ["```(庚辰眉批：", '<code class="gb">']
+    ["```)", "<code>"]
+  ]
+  text = ("<small>").join(text.split("```("))
+  text = ("</small>").join(text.split(")```"))
   text = ('<br>').join(text.split("\n"))
   o.append(text)
   o.append("</>")
